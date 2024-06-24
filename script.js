82baafd7df07c1ab992189444e5e48ffc5b95ef1
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-06-24 11:45:29 +0200

    tt

--- script.js
+++ script.js
@@ -1,51 +1,21 @@
-let myname = "ibtihel"
-myname ="helllo"
-console.log(myname)
-const num= 52.5
-console.log(num * 6)
-console.log(7==9)
-let x = null
-console.log(x)
-const names = ["youssef", "esra", "asma", "nour"]
-for (let i = 0; i< names.length; i++){
-  console.log(names[i])  
-  console.log(names[i].length)
-}
-for (let i of names){
-    console.log(i)
-}
+const h1 = document.querySelector("h1");
 
-const person = {
-    firstName:"yasmine",
-    lastName: "Tebourski",
-    school: 1
-}
-console.log(person)
-console.log(person.lastName)
- 
-const students = [
-    {
-        firstName:"yasmine",
-        lastName: "Tebourski",
-        school: 1
-    },{
-        firstName:"dhia",
-        lastName: "dachraoui",
-        school: 5
-    },{
-        firstName:"khalil",
-        lastName: "ben kram",
-        school: 4
-    }
-]
-if( students[0].firstName == "khalil"){
-    console.log("it's khalil")
-}else if ( students[0].firstName == "dhia"){
-    console.log("it's dhia")    
-}else{
-    console.log("no found")
-}
-function add (num1, num2){
- return num1 +num2
-}
-console.log(add(1,5))
+console.log(h1.textContent);
+const nav = document.querySelector("#nav");
+const moreNews = document.querySelectorAll(".more-news");
+console.log(moreNews);
+const h2 = document.createElement("h2");
+h2.innerHTML = "hello";
+h2.style.backgroundColor = "red";
+h2.style.color = "white";
+document.querySelector(".child2").appendChild(h2);
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("bgc");
+});
+moreNews.forEach(function (item, i) {
+  item.addEventListener("click", function (e) {
+    const target = e.target;
+    moreNews[i].classList.toggle("bgc");
+  });
+});
